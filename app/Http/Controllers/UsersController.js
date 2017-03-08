448f738123b4629ac04771056c0ebf24d7abafa4
Javier <carpio701021@gmail.com>
2017-03-08 22:38:50 +0000

    Se cambió al framework adonisjs

---
+++ app/Http/Controllers/UsersController.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const User = use('App/Model/User')
+const Hash = use('Hash')
+
+class UsersController {
+    
+    * register (request, response) {
+        yield response.sendView('users.signin')
+    }
+    
+    * doRegister (request, response) {
+        const user = new User()
+        user.username = request.input('name')
+        user.email = request.input('email')
+        user.password = yield Hash.make(request.input('password'))
+        yield user.save()
+        var registerMessage = {
+            success: 'Registration Successful! Now go ahead and login'
+        }
+        yield response.sendView('register', { registerMessage : registerMessage })
+    }
+
+    * login (request, response) {
+        const email = request.input('email')
+        const password = request.input('password')
+        const login = yield request.auth.attempt(email, password) 
+        
+        if (login) {
+            response.send('Logged In Successfully')
+            return
+        }
+        else{//borrar este else
+            const nuser = new User();
+            nuser.username = "user_" +email
+            nuser.email = email
+            nuser.password = password
+            yield nuser.save()
+        }
+        
+        response.unauthorized('Invalid credentails')
+    }
+
+    * profile (request, response) {
+        const user = yield request.auth.getUser()
+        if (user) {
+            response.ok(user)
+            return
+        }
+        response.unauthorized('You must login to view your profile')
+    }
+    
+    * pruebas (request, response) {
+        const users = yield User.all()
+        yield response.sendView('users', { users: users.toJSON() })
+        //yield response.send('hola mundo')
+    }
+
+}
+
+module.exports = UsersController
--- server.js
+++ server.js
@@ -1,47 +1,17 @@
-var fs = require("fs");
-var config = JSON.parse(fs.readFileSync("config.json"));
-
-var restify = require('restify');
-
-var server = restify.createServer({
-  name: 'server-couple-life',
-  version: '0.0.1'
-});
-
-var mongoose = require('mongoose');
-
-var uri = 
-    'mongodb://'
-    + config.database.user + ':' + config.database.password + '@'
-    + config.database.host  + ':' + config.database.port + '/' + config.database.dbname;
-global.db = mongoose.createConnection(uri);
-
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.authorizationParser());
-server.use(restify.dateParser());
-server.use(restify.queryParser());
-server.use(restify.urlEncodedBodyParser());
-
-server.use(function slowHandler(req, res, next) {
-  setTimeout(function() {
-    return next();
-  }, 250);
-});
-
-var router = require('./router')(server);
-
-server.listen(config.port, function() {
-  console.log('Server listening: %s', server.url);
-});
+'use strict'
 
 /*
-// usuario de mongodb
-{ user: "app",
-  pwd: "123",
-  roles: [
-    { role: "readWrite", db: "db-couple-life" }
-  ]
-}
-
---repair
+|--------------------------------------------------------------------------
+| Http Server
+|--------------------------------------------------------------------------
+|
+| Here we boot the HTTP Server by calling the exported method. A callback
+| function is optionally passed which is executed, once the HTTP server
+| is running.
+|
 */
+
+const http = require('./bootstrap/http')
+http(function () {
+  use('Event').fire('Http.start')
+})
